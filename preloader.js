c24d6c9a50e2de094e4a0a29847c8693e6904cc3
Big-Fat-Dino <88196819+Big-Fat-Dino@users.noreply.github.com>
2021-08-07 23:01:02 +0100

    Delete preloader.js

--- preloader.js
+++
@@ -1,4 +0,0 @@
-var ipcRenderer = require('electron').ipcRenderer;
-document.addEventListener("DOMContentLoaded", function () {
-   ipcRenderer.sendToHost('html-content' , document.body.innerHTML);
-});
